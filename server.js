59cafe4558ead85d177d6b1bdea27da428522a08
27abhishekcodes <27abhishekcool@gmail.com>
2026-03-04 19:33:20 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 const app=require("./src/app")
 const connectDB=require("./src/db/db")
+const PORT = process.env.PORT || 3000;
 console.log("JWTsceret" , process.env.JWT_SECRET);
 connectDB();
 
 
 
-app.listen(3000,()=>{
-    console.log("server is listening to port 3000")
+app.listen(PORT,()=>{
+    console.log("server is listening to port")
 }) 
