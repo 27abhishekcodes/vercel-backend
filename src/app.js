fd31456304df3f37d90d064edf0f4a4f6eca91b9
27abhishekcodes <27abhishekcool@gmail.com>
2026-03-04 23:06:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app=express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://vercel-frontend-eta-six.vercel.app" ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
